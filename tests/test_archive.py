4ed775cfe98693aacb9782b7fe0f65e9c8490b16
Kornilin5 <sasha_kornilin@mail.ru>
2024-02-01 16:42:32 +0400

    форматировал код

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -6,14 +6,18 @@ from openpyxl import load_workbook
 from pypdf import PdfReader
 import csv
 
+
 def create_archive(file_names, archive_path, tmp_path):
-    files_to_zip = [os.path.join(tmp_path, file_name) for file_name in file_names]
+    files_to_zip = [
+        os.path.join(tmp_path, file_name) for file_name in file_names
+    ]
     zip_file_name = os.path.join(archive_path, 'achive_test.zip')
 
     with zipfile.ZipFile(zip_file_name, 'w') as zip_file:
         for file in files_to_zip:
             zip_file.write(file, os.path.relpath(file, tmp_path))
     print(f'Zip-архив "{zip_file_name}" создан успешно.')
+
 
 create_archive(NAME_FILES, RESOURCES_PATH, TMP_PATH)
 
@@ -29,13 +33,18 @@ def read_csv(path_archive, folder_path, name_file):
     path = os.path.join(folder_path, name_file)
     target_text = 'eto test'
     with open(path, encoding='utf-8') as csvfile:
-        found_rows = [row for row in csv.reader(csvfile, delimiter=';') if any(target_text in value for value in row)]
+        found_rows = [
+            row for row in csv.reader(csvfile, delimiter=';')
+            if any(target_text in value for value in row)
+        ]
     for row in found_rows:
         print(f"Найден текст '{target_text}'")
 
-read_csv(RESOURCES_PATH,RESOURCES_PATH, 'это_csv.csv')
 
-def read_xlsx_file(path_archive, folder_path,  name_file):
+read_csv(RESOURCES_PATH, RESOURCES_PATH, 'это_csv.csv')
+
+
+def read_xlsx_file(path_archive, folder_path, name_file):
     extract_file_from_zip(path_archive, folder_path, name_file)
     path = os.path.join(folder_path, name_file)
     open_xlsx = load_workbook(path)
@@ -44,7 +53,8 @@ def read_xlsx_file(path_archive, folder_path,  name_file):
     text_found = "Регистрация пользователя на сайте при вводе корректного пароля"
     assert text == text_found
 
-read_xlsx_file(RESOURCES_PATH,RESOURCES_PATH, 'эксель.xlsx')
+
+read_xlsx_file(RESOURCES_PATH, RESOURCES_PATH, 'эксель.xlsx')
 
 
 def read_pdf(path_archive, folder_path, name_file):
